f07612955875946b4cf3568c5dda35e03c09dd61
dorrelredmond <dorrelredmond@gmail.com>
2025-07-13 12:40:12 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
 
     # Home Manager Configurations for Unix Systems
     homeConfigurations = {
-      dorrel = home-manager.lib.homeManagerConfiguration {
+      ${user} = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = {
         inherit inputs user;
