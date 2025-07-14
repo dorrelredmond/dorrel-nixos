b0a24fd3d764306ad778682b8037853c461bba7b
dorrelredmond <dorrelredmond@gmail.com>
2025-07-14 09:04:51 -0500

    enable hyprland home-manager configs

--- flake.nix
+++ flake.nix
@@ -57,12 +57,12 @@
           ./modules/home/home.nix
 
           # Hyprland Configs
-          #wayland.windowManager.hyprland = {
-          #  enable = true;
-          #  # set the flake package
-          #  package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-          #  portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-          #};
+          wayland.windowManager.hyprland = {
+            enable = true;
+            # set the flake package
+            package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+            portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+          };
         ];
       };
     };
