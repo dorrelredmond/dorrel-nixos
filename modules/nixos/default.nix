6b00a28af16f6eeae887dc122c0da3e2f888b1bd
dorrelredmond <dorrelredmond@gmail.com>
2025-08-05 09:54:05 -0500

    default editor as neovim

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -100,6 +100,7 @@
 
     # Environment Variables
     environment.variables.QT_QPA_PLATFORMTHEME = "qt5ct";
+    environment.variables.EDITOR = "nvim";
 
     # Hyprland Window Manager Configuration
     programs.hyprland = {
