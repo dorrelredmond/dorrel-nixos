2fc2f71a44f27ddcf04e992c8c51317edf56f7b9
dorrelredmond <dorrelredmond@gmail.com>
2025-08-05 15:37:29 -0500

    update settings

--- modules/common/home/programs/firefox.nix
+++ modules/common/home/programs/firefox.nix
@@ -51,6 +51,7 @@
           "privacy.webrtc.legacyGlobalIndicator" = false;
 
           # Actual settings
+          "browser.startup.homepage" = "https://startpage.dorrelredmond.com";
           "sidebar.verticalTabs" = true;
           "app.shield.optoutstudies.enabled" = false;
           "app.update.auto" = false;
@@ -59,15 +60,12 @@
           "browser.ctrlTab.recentlyUsedOrder" = false;
           "browser.discovery.enabled" = false;
           "browser.laterrun.enabled" = false;
-          "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" =
-            false;
-          "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" =
-            false;
+          "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+          "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
           "browser.newtabpage.activity-stream.feeds.snippets" = false;
           "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned" = "";
           "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines" = "";
-          "browser.newtabpage.activity-stream.section.highlights.includePocket" =
-            false;
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
           "browser.newtabpage.activity-stream.showSponsored" = false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
           "browser.newtabpage.pinned" = false;
@@ -88,6 +86,29 @@
           "identity.fxaccounts.enabled" = false;
           "privacy.trackingprotection.enabled" = true;
           "privacy.trackingprotection.socialtracking.enabled" = true;
+          "signon.rememberSignons" = false;
+          "extensions.formautofill.adresses.enabled" = false;
+          "extensions.formautofill.creditCards.enabled" = false;
+          
+          # Telemetry Settings
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.server" = false;
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enable" = false;
+          "experiments.activeExperiment" = false;
+          "experiments.enabled" = false;
+          "experiments.supported" = false;
+          "network.allow-experiments" = false;
       };
 
       extensions = {
