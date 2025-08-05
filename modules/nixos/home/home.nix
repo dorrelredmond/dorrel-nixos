397ee534d6a68767603561107c947ea7c18372ca
dorrelredmond <dorrelredmond@gmail.com>
2025-08-05 13:52:20 -0500

    nur support

--- modules/nixos/home/home.nix
+++ modules/nixos/home/home.nix
@@ -1,7 +1,12 @@
-{ config, nur, pkgs, inputs, user, ... }: {
+{ config, nur, pkgs, inputs, user, lib, ... }: 
+let
+  nur-no-pkgs = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/main.tar.gz") {};
+in
+{
   imports = [
     ./catppuccin.nix
     ../../common/home
+    lib.attrValues nur-no-pkgs.repos.moredhel.hmModules.rawModules;
   ];
   
   nixpkgs.config.allowUnfree = true;
