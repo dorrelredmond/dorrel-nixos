9975bc2080328e2f1e2eb1822910f1e8573b0094
dorrelredmond <dorrelredmond@gmail.com>
2025-08-05 14:58:53 -0500

    remove package settings & standalone options

--- modules/nixos/home/home.nix
+++ modules/nixos/home/home.nix
@@ -4,16 +4,10 @@
     ./catppuccin.nix
     ../../common/home
   ];
-  
-  nixpkgs.config.allowUnfree = true;
 
   home = {
     username = user;
     homeDirectory = "/home/${user}";
     stateVersion = "25.05";
   };
-
-
-  # let Home Manager manage itself when in standalone mode
-  programs.home-manager.enable = true;
 }
