bad9320213942f10562a09bf02f8d3fb953753c5
dorrelredmond <dorrelredmond@gmail.com>
2025-08-05 08:02:13 -0500

    add error handling

--- upgrade.sh
+++ upgrade.sh
@@ -10,24 +10,42 @@ echo -e "\nEnter Home Manager Configuration to use"
 read homeConfig
 
 echo -e "\nNotice\nRequesting Updates: Flake Lock\n"
-nix flake update --commit-lock-file
-echo -e "\nRequest Confirmed\nUpdate: Flake Lock Acquisition Successful"
+if nix flake update --commit-lock-file ; then
+    echo -e "\nRequest Confirmed\nUpdate: Flake Lock Acquisition Successful"
+else
+    echo -e "\nNotice\nAn Error Has Occurred"
+    exit 1
+fi
 
 if [ $installType == "nix" ]
 then
-echo -e "\nNotice\nRequesting Updates: System Packages\n"
-sudo nixos-rebuild switch --flake .#$systemConfig --upgrade
-echo -e "\nUpdates: System Packages Acquisition Successful"
+    echo -e "\nNotice\nRequesting Updates: System Packages\n"
+    
+    if sudo nixos-rebuild switch --flake .#$systemConfig --upgrade; then
+        echo -e "\nRequest Confirmed\nUpdates: System Packages Acquisition Successful"
+    else
+        echo -e "\nNotice\nAn Error Has Occurred"
+        exit 1
+    fi
 
 elif [ $installType == "darwin" ]
 then
-echo -e "\nNotice\nRequesting Updates: System Packages\n"
-darwin-rebuild switch --flake .#$systemConfig --upgrade
-echo -e "\nRequest Confirmed\nUpdates: System Packages Acquisition Successful"
+    echo -e "\nNotice\nRequesting Updates: System Packages\n"
+    
+    if darwin-rebuild switch --flake .#$systemConfig --upgrade; then
+        echo -e "\nRequest Confirmed\nUpdates: System Packages Acquisition Successful"
+    else
+        echo -e "\nNotice\nAn Error Has Occurred"
+        exit 1
+    fi
 fi
 
 echo -e "\nNotice\nRequesting Updates: Home Manager\n"
-home-manager switch --flake .#$homeConfig
-echo -e "\nRequest Confirmed\nUpdates: Home Manager Acquisition Successful"
+if home-manager switch --flake .#$homeConfig; then
+    echo -e "\nRequest Confirmed\nUpdates: Home Manager Acquisition Successful"
+else
+    echo -e "\nNotice\nAn Error Has Occurred"
+    exit 1
+fi
 
 echo -e "\nNotice\nThe system has now been fully upgraded"
