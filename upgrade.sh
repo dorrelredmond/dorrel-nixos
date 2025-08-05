655ceaab9bf5b60361854cd6d8a1279ca15b0d90
dorrelredmond <dorrelredmond@gmail.com>
2025-08-05 06:58:56 -0500

    Add messages before and after each update command

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #! /run/current-system/sw/bin/bash
 
-echo "Are you updating a "nix" or "darwin" install?"
+echo "Are you updating a nix or darwin install?"
 read installType
 
 
@@ -12,9 +12,17 @@ read nixConfig
 echo "Enter Home Manager Configuration to use"
 read homeConfig
 
+echo -e "\nBeginning Flake Lock File Updates\n"
 nix flake update
+echo -e "\nSuccess\n"
+
+echo -e "\nBeginning System Package Updates\n"
 sudo nixos-rebuild switch --flake .#$nixConfig --upgrade
+echo -e "\nSuccess\n"
+
+echo -e "\nBeginning Home Manager Updates\n"
 home-manager switch --flake .#$homeConfig
+echo -e "\nSuccess\n"
 
 elif [ $installType == "darwin" ]
 then
@@ -24,8 +32,16 @@ read darwinConfig
 echo "Enter Home Manager Configuration to use"
 read homeConfig
 
+echo -e "\nBeginning Flake Lock File Updates\n"
 nix flake update
+echo -e "\nSuccess\n"
+
+echo -e "\nBeginning System Package Updates\n"
 darwin-rebuild switch --flake .#$darwin --upgrade
+echo -e "\nSuccess\n"
+
+echo -e "\nBeginning Home Manager Updates\n"
 home-manager switch --flake .#$homeConfig
+echo -e "\nSuccess\n"
 
 fi
