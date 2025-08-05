0ac3aa22efab8923c6da4b74e2262bd63714e802
dorrelredmond <dorrelredmond@gmail.com>
2025-08-05 07:19:57 -0500

    simplify script

--- upgrade.sh
+++ upgrade.sh
@@ -3,45 +3,31 @@
 echo -e "\nAre you updating a nix or darwin install?"
 read installType
 
+echo -e "\nEnter System Configuration to use (i.e., desktop or macbook)"
+read systemConfig
+
+echo -e "\nEnter Home Manager Configuration to use"
+read homeConfig
+
+echo -e "\nBeginning Flake Lock File Updates"
+nix flake update --commit-lock-file
+echo -e "\nSuccess"
 
 if [ $installType == "nix" ]
 then
-echo -e "\nEnter NixOS Configuration to use"
-read nixConfig
-
-echo -e "\nEnter Home Manager Configuration to use"
-read homeConfig
-
-echo -e "\nBeginning Flake Lock File Updates\n"
-nix flake update
-echo -e "\nSuccess\n"
-
-echo -e "\nBeginning System Package Updates\n"
-sudo nixos-rebuild switch --flake .#$nixConfig --upgrade
-echo -e "\nSuccess\n"
-
-echo -e "\nBeginning Home Manager Updates\n"
-home-manager switch --flake .#$homeConfig
-echo -e "\nSuccess\n"
+echo -e "\nBeginning System Package Updates"
+sudo nixos-rebuild switch --flake .#$systemConfig --upgrade
+echo -e "\nSuccess"
 
 elif [ $installType == "darwin" ]
 then
-echo -e "\nEnter Darwin Configuration to use"
-read darwinConfig
+echo -e "\nBeginning System Package Updates"
+darwin-rebuild switch --flake .#$systemConfig --upgrade
+echo -e "\nSuccess"
+fi
 
-echo -e "\nEnter Home Manager Configuration to use"
-read homeConfig
-
-echo -e "\nBeginning Flake Lock File Updates\n"
-nix flake update
-echo -e "\nSuccess\n"
-
-echo -e "\nBeginning System Package Updates\n"
-darwin-rebuild switch --flake .#$darwin --upgrade
-echo -e "\nSuccess\n"
-
-echo -e "\nBeginning Home Manager Updates\n"
+echo -e "\nBeginning Home Manager Updates"
 home-manager switch --flake .#$homeConfig
-echo -e "\nSuccess\n"
+echo -e "\nSuccess"
 
 fi
