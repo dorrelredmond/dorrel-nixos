5164ae0f49c11fb7522743aabd8121d985f8cb51
dorrelredmond <dorrelredmond@gmail.com>
2025-08-06 11:13:43 -0500

    define XDG file paths

--- modules/nixos/environment.nix
+++ modules/nixos/environment.nix
@@ -2,4 +2,19 @@
   # Environment Variables
   environment.variables.QT_QPA_PLATFORMTHEME = "qt5ct";
   environment.variables.EDITOR = "nvim";
+
+  # This is using a rec (recursive) expression to set and access XDG_BIN_HOME within the expression
+  # For more on rec expressions see https://nix.dev/tutorials/first-steps/nix-language#recursive-attribute-set-rec
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME    = "$HOME/.local/bin";
+    PATH = [ 
+      "${XDG_BIN_HOME}"
+    ];
+  };
 }
