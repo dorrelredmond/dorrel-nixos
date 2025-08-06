1278936ea8924992979a36c64e0837f2f9a7f59f
dorrelredmond <dorrelredmond@gmail.com>
2025-08-06 13:34:07 -0500

    fix file path

--- modules/common/home/programs/default.nix
+++ modules/common/home/programs/default.nix
@@ -9,6 +9,6 @@
 
   # Set File Locations for configuration files that do not work properly using Nix
   home.file = {
-    ".config/fastfetch/config.jsonc".source = ./config/fastfetch/config.jsonc;
+    ".config/fastfetch/config.jsonc".source = ../config/fastfetch/config.jsonc;
   };
 }
