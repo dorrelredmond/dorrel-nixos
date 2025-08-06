ae4584d02f52eeb554df54d3b3b16a607c94e809
dorrelredmond <dorrelredmond@gmail.com>
2025-08-06 09:09:36 -0500

    Reorganize and disable warnings

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,21 +1,32 @@
 {
-  # Enable Flakes Support
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # substituters
-  nix.settings = {
-      substituters = ["https://hyprland.cachix.org"];
-      trusted-substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
-
-  # Automatic Cleanup
   nix = {
-      gc = {
-          automatic = true;
-          dates = "weekly";
-          options = "--delete-older-than 30d";
-      };
-      settings.auto-optimise-store = true;
+    # Automatic Garbage Cleanup
+    gc = {
+        automatic = true;
+        options = "--delete-older-than 14d";
+    };
+
+    settings = {
+        # Substituers
+        substituters = ["https://hyprland.cachix.org"];
+        trusted-substituters = ["https://hyprland.cachix.org"];
+        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    
+        # automatically optimise symlinks
+        auto-optimise-store = true;
+    };
+
+    experimental-features = [ 
+        # Enable flakes
+        "flakes"
+
+        # Enables the nix3 commands, a requirement for flakes
+        "nix-command"
+    ];
+    
+
+    # Don't warn if the current working tree is dirty
+    # I don't need the warning because I'm working on it right now
+    warn-dirty = false;
   };
 }
