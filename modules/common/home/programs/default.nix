f141fce5c5c397f3a94d5a5ced6c24b4112ed78c
dorrelredmond <dorrelredmond@gmail.com>
2025-08-06 13:32:41 -0500

    source original fastfetch config

--- modules/common/home/programs/default.nix
+++ modules/common/home/programs/default.nix
@@ -1,10 +1,14 @@
 {
   imports = [
-    ./fastfetch.nix
     ./firefox.nix
     ./fish.nix
     ./git.nix
     ./kitty.nix
     ./starship.nix
   ];
+
+  # Set File Locations for configuration files that do not work properly using Nix
+  home.file = {
+    ".config/fastfetch/config.jsonc".source = ./config/fastfetch/config.jsonc;
+  };
 }
