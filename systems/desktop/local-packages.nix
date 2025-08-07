96f6af657122b4431cc751f504c3cf48823b2879
dorrelredmond <dorrelredmond@gmail.com>
2025-08-07 07:49:29 -0500

    fix file path

--- systems/desktop/local-packages.nix
+++ systems/desktop/local-packages.nix
@@ -15,7 +15,7 @@
   };
 
   environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/user/.steam/root/compatibilitytools.d";
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/dorrel/.steam/root/compatibilitytools.d";
   };
 
   # General Packages
