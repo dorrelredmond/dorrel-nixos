a8643011efa0bb604cf2e41139b3d8824ba7f6c6
dorrelredmond <dorrelredmond@gmail.com>
2025-08-22 22:46:39 -0500

    source configs individually

--- modules/common/home/default.nix
+++ modules/common/home/default.nix
@@ -4,8 +4,30 @@
   ];
 
   # Set File Locations for configuration files that do not work or are more difficult to setup with Nix
-  home.file."${config.xdg.configHome}" = {
-    source = ./config;
-    recursive = true;
+  home.file = {
+    "bat" = {
+      source = ./config/bat;
+      recursive = true;
+    };
+    "btop" = {
+      source = ./config/btop;
+      recursive = true;
+    };
+    "fastfetch" = {
+      source = ./config/fastfetch;
+      recursive = true;
+    };
+    "mpv" = {
+      source = ./config/mpv;
+      recursive = true;
+    };
+    "nvim" = {
+      source = ./config/nvim;
+      recursive = true;
+    };
+    "ranger" = {
+      source = ./config/ranger;
+      recursive = true;
+    };
   };
 }
--- modules/nixos/home/home.nix
+++ modules/nixos/home/home.nix
@@ -13,8 +13,61 @@
   };
 
   # Set File Locations for configuration files that do not work or are more difficult to setup with Nix
-  home.file."${config.xdg.configHome}" = {
-    source = ./config;
-    recursive = true;
+  home.file = {
+    "calibre" = {
+      source = ./config/calibre;
+      recursive = true;
+    };
+    "hypr" = {
+      source = ./config/hypr;
+      recursive = true;
+    };
+    "Kvantum" = {
+      source = ./config/Kvantum;
+      recursive = true;
+    };
+    "pipewire" = {
+      source = ./config/pipewire;
+      recursive = true;
+    };
+    "qBittorrent" = {
+      source = ./config/qBittorrent;
+      recursive = true;
+    };
+    "qt5ct" = {
+      source = ./config/qt5ct;
+      recursive = true;
+    };
+    "qt6ct" = {
+      source = ./config/qt6ct;
+      recursive = true;
+    };
+    "solaar" = {
+      source = ./config/solaar;
+      recursive = true;
+    };
+    "strawberry" = {
+      source = ./config/strawberry;
+      recursive = true;
+    };
+    "swappy" = {
+      source = ./config/swappy;
+      recursive = true;
+    };
+    "swaync" = {
+      source = ./config/swaync;
+      recursive = true;
+    };
+    "waybar" = {
+      source = ./config/waybar;
+      recursive = true;
+    };
+    "zathura" = {
+      source = ./config/zathura;
+      recursive = true;
+    };
+    "rofimoji.rc" = {
+      source = ./config/rofimoji.rc;
+    };
   };
 }
