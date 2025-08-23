953a1c12b2eabcdbc27169e2f7341171a54efcb2
dorrelredmond <dorrelredmond@gmail.com>
2025-08-22 22:22:58 -0500

    fix file path

--- modules/nixos/home/home.nix
+++ modules/nixos/home/home.nix
@@ -13,7 +13,7 @@
   };
 
   # Set File Locations for configuration files that do not work or are more difficult to setup with Nix
-  home.file."config" = {
+  home.file."${config.xdg.configHome}" = {
     source = ./config;
     recursive = true;
   };
