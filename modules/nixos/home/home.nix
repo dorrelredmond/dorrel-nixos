b6be3283a5844175e4218bf7078ed68c61df89f8
dorrelredmond <dorrelredmond@gmail.com>
2025-08-31 22:55:07 -0500

    nixos home manger file paths

--- modules/nixos/home/home.nix
+++ modules/nixos/home/home.nix
@@ -1,4 +1,7 @@
 { config, pkgs, inputs, user, ... }: 
+let 
+  configPath = "/home/dorrel/dorrel-nixos/modules/nixos/home";
+in 
 {
   imports = [
     ./catppuccin.nix
@@ -15,59 +18,59 @@
   # Set File Locations for configuration files that do not work or are more difficult to setup with Nix
   home.file = {
     ".config/calibre" = {
-      source = ./config/calibre;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/calibre";
       recursive = true;
     };
     ".config/hypr" = {
-      source = ./config/hypr;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/hypr";
       recursive = true;
     };
     ".config/Kvantum" = {
-      source = ./config/Kvantum;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/Kvantum";
       recursive = true;
     };
     ".config/pipewire" = {
-      source = ./config/pipewire;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/pipewire";
       recursive = true;
     };
     ".config/qBittorrent" = {
-      source = ./config/qBittorrent;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/qBittorrent";
       recursive = true;
     };
     ".config/qt5ct" = {
-      source = ./config/qt5ct;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/qt5ct";
       recursive = true;
     };
     ".config/qt6ct" = {
-      source = ./config/qt6ct;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/qt6ct";
       recursive = true;
     };
     ".config/solaar" = {
-      source = ./config/solaar;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/solaar";
       recursive = true;
     };
     ".config/strawberry" = {
-      source = ./config/strawberry;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/strawberry";
       recursive = true;
     };
     ".config/swappy" = {
-      source = ./config/swappy;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/swappy";
       recursive = true;
     };
     ".config/swaync" = {
-      source = ./config/swaync;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/swaync";
       recursive = true;
     };
     ".config/waybar" = {
-      source = ./config/waybar;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/waybar";
       recursive = true;
     };
     ".config/zathura" = {
-      source = ./config/zathura;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/zathura";
       recursive = true;
     };
     ".config/rofimoji.rc" = {
-      source = ./config/rofimoji.rc;
+      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/config/rofimoji.rc";
     };
   };
 }
