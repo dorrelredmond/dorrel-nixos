04797bba23dbbf00d0a20c1423246825034f5f58
dorrelredmond <dorrelredmond@gmail.com>
2025-08-31 23:53:05 -0500

    home manager is so annoying

--- modules/common/home/default.nix
+++ modules/common/home/default.nix
@@ -2,32 +2,4 @@
   imports = [
     ./programs
   ];
-
-  # Set File Locations for configuration files that do not work or are more difficult to setup with Nix
-  home.file = {
-    "bat" = {
-      source = ./config/bat;
-      recursive = true;
-    };
-    "btop" = {
-      source = ./config/btop;
-      recursive = true;
-    };
-    "fastfetch" = {
-      source = ./config/fastfetch;
-      recursive = true;
-    };
-    "mpv" = {
-      source = ./config/mpv;
-      recursive = true;
-    };
-    "nvim" = {
-      source = ./config/nvim;
-      recursive = true;
-    };
-    "ranger" = {
-      source = ./config/ranger;
-      recursive = true;
-    };
-  };
 }
