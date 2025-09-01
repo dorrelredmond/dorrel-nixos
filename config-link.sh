dbfcab6bc067a83d31eabbf71b227d8afd90607a
dorrelredmond <dorrelredmond@gmail.com>
2025-09-01 00:33:02 -0500

    script to symlink dotfiles

---
+++ config-link.sh
@@ -0,0 +1,38 @@
+#! /run/current-system/sw/bin/bash
+
+echo -e "n\Notice\nAre you linking nix or darwin configs?"
+read configType
+    
+cd ~/.config
+
+echo -e "\nNotice\nRequesting Update: Common Links"
+
+if ln -sf ~/dorrel-nixos/modules/common/home/config/* ./ ; then
+    echo -e "Request Confirmed\nUpdate: Common Links Acquisition Successful"
+else
+    echo -e "\nNotice\nAn Error Has Occurred"
+    exit 1
+fi
+
+echo -e "\nNotice\nRequesting Desired Links"
+
+if [ $configType == "nix" ]; then
+    if ln -sf ~/dorrel-nixos/modules/nixos/home/config/* ./ ; then
+        echo -e "Request Confirmed\nUpdate: Nix Links Acquisition Successful"
+    else
+        echo -e "\nNotice\nAn Error Has Occurred"
+        exit 1
+    fi
+elif [ $configType == "darwin" ]; then
+    if ln -sf ~/dorrel-nixos/modules/darwin/home/config/* ./ ; then
+        echo -e "Request Confirmed\nUpdate: Darwin Links Acquisition Successful"
+    else
+        echo -e "\nNotice\nAn Error Has Occurred"
+        exit 1
+    fi
+else
+    echo -e "\nNotice\nAn Error Has Occurred"
+    exit 1
+fi
+
+echo -e "\nNotice\nAll Configurations Have Been Set"
