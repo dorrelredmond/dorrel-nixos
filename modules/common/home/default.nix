32eea3e64026bb66f8be5c8c91bda11fe6c00dc9
dorrelredmond <dorrelredmond@gmail.com>
2025-08-31 23:50:19 -0500

    I hate home manager

--- modules/common/home/default.nix
+++ modules/common/home/default.nix
@@ -2,32 +2,4 @@
   imports = [
     ./programs
   ];
-
-  # Set File Locations for configuration files that do not work or are more difficult to setup with Nix
-  home.file = {
-    ".config/bat" = {
-      source = ./config/bat;
-      recursive = true;
-    };
-    ".config/btop" = {
-      source = ./config/btop;
-      recursive = true;
-    };
-    ".config/fastfetch" = {
-      source = ./config/fastfetch;
-      recursive = true;
-    };
-    ".config/mpv" = {
-      source = ./config/mpv;
-      recursive = true;
-    };
-    ".config/nvim" = {
-      source = ./config/nvim;
-      recursive = true;
-    };
-    ".config/ranger" = {
-      source = ./config/ranger;
-      recursive = true;
-    };
-  };
 }
