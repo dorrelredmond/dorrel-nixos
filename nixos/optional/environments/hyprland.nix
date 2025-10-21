011e87e01034df323c7c494c1be57d86a606381b
dorrelredmond <dorrelredmond@gmail.com>
2025-10-20 20:28:19 -0500

    syntax error

--- nixos/optional/environments/hyprland.nix
+++ nixos/optional/environments/hyprland.nix
@@ -26,6 +26,7 @@
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
+  };
 
   #security Configuration
   security.pam.services.hyprlock = {};
