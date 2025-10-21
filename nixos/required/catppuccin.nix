81a75b3ad09332dadfb02ddea290078340c6d6bc
dorrelredmond <dorrelredmond@gmail.com>
2025-10-20 20:24:09 -0500

    temp disable stylix

--- nixos/required/catppuccin.nix
+++ nixos/required/catppuccin.nix
@@ -10,6 +10,6 @@
     catppuccinifier-cli
   ];
 
-  stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes/share/themes/catppuccin-mocha.yaml}";
+  #stylix.enable = true;
+  #stylix.base16Scheme = "${pkgs.base16-schemes/share/themes/catppuccin-mocha.yaml}";
 }
