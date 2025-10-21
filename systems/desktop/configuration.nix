ccbe8b9d42eeb17fa71e523941b7eabe669ee061
dorrelredmond <dorrelredmond@gmail.com>
2025-10-20 20:19:09 -0500

    implement sway

--- systems/desktop/configuration.nix
+++ systems/desktop/configuration.nix
@@ -10,7 +10,8 @@
     # Display Manager & Desktop Environment
     ../../nixos/optional/loginManager/sddm.nix
     ../../nixos/optional/environments/hyprland.nix
-    ../../nixos/optional/environments/kde.nix
+    ../../nixos/optional/environments/gnome.nix
+    ../../nixos/optional/environments/sway.nix
     ../../nixos/optional/apps/qt-apps.nix
     ../../nixos/optional/apps/wayland-apps.nix
     
