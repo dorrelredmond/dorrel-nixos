e3aae7ca3a06f999fb900b7dfe095cba20462b86
dorrelredmond <dorrelredmond@gmail.com>
2025-10-30 17:56:58 -0500

    add strawberry back

--- nixos/optional/apps/qt-apps.nix
+++ nixos/optional/apps/qt-apps.nix
@@ -4,6 +4,7 @@
     # Music & Audio
     kdePackages.k3b # CD/DVD burning & ripping
     kid3-qt # audio tag editor
+    strawberry # music player
     
     # Communication
     kdePackages.konversation # IRC client
