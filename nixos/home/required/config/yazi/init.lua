2bc2b0c5c84ceb849dd9158aee6e6707450527da
dorrelredmond <dorrelredmond@gmail.com>
2025-11-04 17:47:57 -0600

    Manually configure yazi
    switch from using nix to configure yazi to a more manual setup for the
    sake of ease while I continue learning nix

---
+++ nixos/home/required/config/yazi/init.lua
@@ -0,0 +1,10 @@
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.PLAIN,
+}
+
+require("recycle-bin"):setup()
+
+require("git"):setup()
+
+
