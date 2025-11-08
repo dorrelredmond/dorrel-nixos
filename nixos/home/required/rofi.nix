0b26925dbee388a551137a5c33c3807fe947993d
dorrelredmond <dorrelredmond@gmail.com>
2025-11-08 14:17:20 -0600

    syntax error
    fix syntax errors

--- nixos/home/required/rofi.nix
+++ nixos/home/required/rofi.nix
@@ -2,6 +2,7 @@
     home.file = {
         ".config/rofi" = {
             source = config.lib.file.mkOutOfStoreSymlink "/home/dorrel/dorrel-nixos/nixos/home/required/config/rofi";
+        };
         ".config/rofimoji.rc" = {
             source = config.lib.file.mkOutOfStoreSymlink "/home/dorrel/dorrel-nixos/nixos/home/required/config/rofimoji.rc";
         };
