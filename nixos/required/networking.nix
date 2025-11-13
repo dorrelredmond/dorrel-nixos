cb49809341a101cedc932557ca7b287b4906261c
dorrelredmond <dorrelredmond@gmail.com>
2025-11-13 17:54:31 -0600

    enable iwd
    enable iwd for impala backend

--- nixos/required/networking.nix
+++ nixos/required/networking.nix
@@ -5,6 +5,9 @@
   # Cloudfare DNS Servers and Google DNS as secondary
   networking.nameservers = ["1.1.1.1" "8.8.8.8"];
 
+  # iNet Wireless Daemon
+  networking.wireless.iwd.enable = true;
+
   # Open Ports for Local Send
   networking.firewall = {
     allowedTCPPorts = [53317];
