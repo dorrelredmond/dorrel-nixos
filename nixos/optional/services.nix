9fc7478c8aed5665e77f7065b3ad87870427a55c
dorrelredmond <dorrelredmond@gmail.com>
2025-11-21 12:43:35 -0600

    more media services
    add media services

--- nixos/optional/services.nix
+++ nixos/optional/services.nix
@@ -1,9 +1,29 @@
 {pkgs, ...}: {
+  # Indexer Manager
+  services.prowlarr = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  # TV and Anime Collection Manager
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  # Movie Collection Manager
+  services.radarr = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  # Home Media Server
   services.jellyfin = {
     enable = true;
     openFirewall = true;
   };
 
+  # Additional Service Related Packages
   environment.systemPackages = with pkgs; [
     jellyfin
     jellyfin-web
