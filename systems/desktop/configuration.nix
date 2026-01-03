83471eae2332be48230eef3efa581941aba654c7
dorrelredmond <dorrelredmond@gmail.com>
2026-01-03 14:59:17 -0600

    gaming mouse support
    add support for configuring a gaming mouse

--- systems/desktop/configuration.nix
+++ systems/desktop/configuration.nix
@@ -24,9 +24,13 @@
   # Enable Logitech Hardware Support
   hardware.logitech.wireless.enable = true;
 
+  # Enable ratbagd mouse support for MMO mouse
+  services.ratbagd.enable = true;
+
   # Machine Specific Packages
   environment.systemPackages = with pkgs; [
     solaar # logitech unified receiver settings
+    piper # gtk frontend for ratbagd mouse configs
   ];
 
   system.stateVersion = "25.05";
