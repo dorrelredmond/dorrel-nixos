eb7449c8300496ab371ed7e990fd2645113f70bb
dorrelredmond <dorrelredmond@gmail.com>
2026-02-25 07:28:38 -0600

    remove unneccesary packages and swap to current stable nixpkgs
    remove unneeded media packages and swap to current stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,60 +3,24 @@
 
   inputs = {
     # Primary Package Supplier
-    #
-    # you may also notice that I don't use a `github:` url for nixpkgs this is
-    # beacuse we can save 15mb of data by using the channel tarball this is not
-    # a major saving but it is nice to have
-    # https://deer.social/profile/did:plc:mojgntlezho4qt7uvcfkdndg/post/3loogwsoqok2w
-    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
-
-    # Improved Hardware Support
-    nixos-hardware = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixos-hardware";
-    };
+    nixpkgs.url = "nixpkgs/nixos-25.11";
 
     # Manager Userspace With Nix
     home-manager = {
-      type = "github";
-      owner = "nix-community";
-      repo = "home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    ### Media Tools
-
-    # customize spotify
-    spicetify-nix = {
-      type = "github";
-      owner = "Gerg-L";
-      repo = "spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # YouTube in the terminal
-    yt-x = {
-      type = "github";
-      owner = "Benexl";
-      repo = "yt-x";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     ### Theme Management
     catppuccin = {
-      type = "github";
-      owner = "catppuccin";
-      repo = "nix";
+      url = "github:catppuccin/nix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
   outputs = {
     self,
     nixpkgs,
-    nixos-hardware,
     home-manager,
     catppuccin,
     ...
@@ -72,7 +36,6 @@
           specialArgs = {inherit inputs user;};
           modules = [
             catppuccin.nixosModules.catppuccin
-            nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
 
             ./systems/desktop/configuration.nix
 
--- nixos/required/media.nix
+++ nixos/required/media.nix
@@ -1,16 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-}: let
-  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-in {
-  imports = [
-    inputs.spicetify-nix.nixosModules.spicetify
-  ];
-
+{pkgs, ...}: {
   # Media Packages
   environment.systemPackages = with pkgs; [
     # Reading
@@ -25,24 +13,6 @@ in {
 
     # Video
     mpv
-    inputs.yt-x.packages."${system}".default # YouTube in the Terminal
     ani-cli # CLI tool to browse and play anime
   ];
-
-  # Spicetify Configuration
-  programs.spicetify = {
-    enable = true;
-    enabledExtensions = with spicePkgs.extensions; [
-      beautifulLyrics
-      autoSkipExplicit
-      fullAppDisplay
-      volumePercentage
-    ];
-    enabledCustomApps = with spicePkgs.apps; [
-      marketplace # strictly used to browse available options easier
-    ];
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
-    alwaysEnableDevTools = true;
-  };
 }
